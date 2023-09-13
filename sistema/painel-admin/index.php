2e01c78a620bb27914831bd6e82aebe90c962274
ageuribeiro <ageu87@gmail.com>
2023-09-13 07:55:12 -0300

    alteração mo estilo

--- sistema/painel-admin/index.php
+++ sistema/painel-admin/index.php
@@ -1,24 +1,115 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css" media="all">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="../../img/logotipo/simbolo-preto.png" type="image/x-icon">
     <title>Painel Administrativo | Bem Chique</title>
 </head>
+
 <body>
-    <nav class="sidebar">
+    <nav class="sidebar close">
         <header class="image-text">
             <span class="image">
-                <img src="../../img/logo_bemchique.jpg" alt="logotipo">
+                <img src="../../img/logo_bemchique.jpg" alt="logo">
             </span>
 
             <div class="text header-text">
                 <span class="name"> Bem Chique</span>
-                <span class="slogan">Moda Elegante</span>
+                <span class="slogan"> Moda Elegante</span>
             </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
         </header>
+        <div class="menu-bar">
+            <div class="menu">
+                <li class="search-box">
+                    <a href="#">
+                        <i class='bx bx-search icon'></i>
+                        <input type="text" placeholder="Search...">
+                    </a>
+                </li>
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-home-alt icon'></i>
+                            <span class="text nav-text">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bar-chart-alt-2 icon'></i>
+                            <span class="text nav-text">
+                                Revenue
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bell icon'></i>
+                            <span class="text nav-text">
+                                Notifications
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-pie-chart-alt icon'></i>
+                            <span class="text nav-text">
+                                Analytics
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon'></i>
+                            <span class="text nav-text">
+                                Likes
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-wallet icon'></i>
+                            <span class="text nav-text">
+                                Wallets
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottom-content">
+                <li class="">
+                    <a href="../index.php">
+                        <i class='bx bx-log-out icon'></i>
+                        <span class="text nav-text"> Logout </span>
+                    </a>
+                </li>
+                <li class="mode">
+                    <div class="moon-sun">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark Mode</span>
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </div>
+        </div>
     </nav>
+    <section class="home">
+        <div class="text">
+            <h1>Dashboard</h1>
+        </div>
+    </section>
+    <script type="text/javascript" src="../js/script.js"></script>
 </body>
+
 </html>
